d565618fc761bac39daf7ae5a3f49bd8a706ec61
Omri Cohen <omric12@gmail.com>
2020-12-30 19:23:05 +0200

    op

--- GoNature Client/src/Client/ClientUI.java
+++ GoNature Client/src/Client/ClientUI.java
@@ -1,82 +1,73 @@
-
 package Client;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import java.util.Vector;
-
-
+import Controller.*;
 import Controller.EmployeeController;
 import Controller.OrderController;
 import Controller.UserController;
-
-import Controller.*;
-
 import GUI.*;
+import java.util.Vector;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 public class ClientUI extends Application {
-	public static ClientController chat; // only one instance
-	public static baseGuiController aFrame;
-	public static EntranceParkController entranceParkController;
-	public static loginClientController cp;
-	public static OrderScreenController oc;
-	public static EnterParkNowController ep;
-	public static WelcomeTravellerController wt;
-	public static WelcomeAndLoginController welcomeController;
-	public static WelcomeTravellerController welcomeTraveller;
-	public static loginClientController LoginClientController;
-	public static OrderScreenController orderScreenController;
-	public static OrderController orderController;
-	public static ParkController parkController;
-	public static EmployeeController employeeController;
-	public static UserController userController;
-	public static DiscountController discountController;
-	public static WaitingListController waitingListController;
-	//public static UserController userController;
 
-	// public static UserController userController;
-	public static SignUpController signUpController;
-	public static SignUpScreenController signUpScreenController;
+  public static ClientController chat; // only one instance
+  public static baseGuiController aFrame;
+  public static EntranceParkController entranceParkController;
+  public static loginClientController cp;
+  public static OrderScreenController oc;
+  public static EnterParkNowController ep;
+  public static WelcomeTravellerController wt;
+  public static WelcomeAndLoginController welcomeController;
+  public static WelcomeTravellerController welcomeTraveller;
+  public static loginClientController LoginClientController;
+  public static OrderScreenController orderScreenController;
+  public static OrderController orderController;
+  public static ParkController parkController;
+  public static EmployeeController employeeController;
+  public static UserController userController;
+  public static DiscountController discountController;
+  public static WaitingListController waitingListController;
+  // public static UserController userController;
 
-	public static RequestsController requestsController;
-	public static ReportsController reportsController;
-	public static ReportEntreisScreenController reportsScreenController;
+  // public static UserController userController;
+  public static SignUpController signUpController;
+  public static SignUpScreenController signUpScreenController;
 
+  public static RequestsController requestsController;
+  public static ReportsController reportsController;
+  public static ReportEntreisScreenController reportsScreenController;
 
+  public static void main(String args[]) throws Exception {
+    launch(args);
+  } // end main
 
-	public static void main(String args[]) throws Exception {
-		launch(args);
-	} // end main
+  public static void set(String ip, int port) {
+    chat = new ClientController(ip, port);
+  }
 
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    cp = new loginClientController();
+    discountController = new DiscountController();
+    waitingListController = new WaitingListController();
+    employeeController = new EmployeeController();
+    parkController = new ParkController();
+    entranceParkController = new EntranceParkController();
+    LoginClientController = new loginClientController();
+    orderScreenController = new OrderScreenController();
+    wt = new WelcomeTravellerController();
+    orderController = new OrderController();
+    signUpScreenController = new SignUpScreenController();
+    signUpController = new SignUpController();
+    welcomeController = new WelcomeAndLoginController();
+    userController = new UserController();
 
-	public static void set(String ip, int port) {
-		chat = new ClientController(ip, port);
-	}
-	@Override 
-	public void start(Stage primaryStage) throws Exception {
-		cp = new loginClientController();
-		discountController=new DiscountController();
-		waitingListController = new WaitingListController();
-		employeeController = new EmployeeController();
-		parkController=new ParkController();
-		entranceParkController=new EntranceParkController();
-		LoginClientController= new loginClientController();
-		orderScreenController = new OrderScreenController();
-		wt = new WelcomeTravellerController();
-		orderController=new OrderController();
-		signUpScreenController=new SignUpScreenController();
-		signUpController = new SignUpController();
-		welcomeController = new WelcomeAndLoginController();
-		userController = new UserController();
+    requestsController = new RequestsController();
 
-		requestsController=new RequestsController();
+    reportsController = new ReportsController();
+    reportsScreenController = new ReportEntreisScreenController();
 
-		reportsController=new ReportsController() ;
-		reportsScreenController=new ReportEntreisScreenController() ;
-
-		cp.start(primaryStage);
-		
-
-	}
-
+    cp.start(primaryStage);
+  }
 }
